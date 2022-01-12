e1828015d16200c89fc77c256b1f54f2908cbbe9
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-01-11 21:24:08 -0300

    Criação da entidade Livro

---
+++ LivrariaBoaLeitura.API/Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LivrariaBoaLeitura.API.Models
+{
+    public class Livro
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public string Categoria { get; set; }
+    }
+}
